9fca3df41dadf390fbe99fd4fc5120a186c896ed
darkend <hiby.90hou@qq.com>
2018-01-30 00:23:28 +1100

    no pry

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
-require 'sinatra/reloader'
-require 'pry'
+# require 'sinatra/reloader'
+# require 'pry'
 
 # using SendGrid's Ruby Library
 # https://github.com/sendgrid/sendgrid-ruby
